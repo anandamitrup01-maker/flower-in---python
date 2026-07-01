d670ee25cf70f8acd5b60eaf48d2c24920736664
Amit Anand <anandamit.rup01@gmail.com>
2026-07-01 19:00:10 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,31 +1,8 @@
-# /// script
-# requires-python = ">=X.XX" TODO: Update this to the minimum Python version you want to support
-# dependencies = [
-#   TODO: Add any dependencies your script requires
-# ]
-# ///
-
-# TODO: Update the main function to your needs or remove it.
-
 
 def main() -> None:
+    print("Start coding in Python today!")
     
-    # import turtle
-    # import colorsys
-    # t = turtle.Turtle()
-    # s = turtle.Screen() . bgcolor('black')
-    # t.speed(0)
-    # n = 70
-    # h = 0
-    # for i in range (360):
-    #     c = colorsys.hsv_to_rgb(h,1,0.8)
-    #     h+= 1/n
-    #     t.color(c)
-    #     t.left(1)
-    #     t.fd(1)
-    #     for j in range (2):
-    #         t.left(2)
-    #         t.circle(200)
+
 
 from turtle import *
 import colorsys
@@ -45,4 +22,4 @@ for i in range (16):
         circle(40, 2.5)
 
 # if __name__ == "__main__":
-    main()
+    # main()
